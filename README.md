92e32e8e123ab8407d3cd98862c2564d0a8f67de
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-02-25 17:33:19 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::WWW::Stub - It's new $module
+
+# SYNOPSIS
+
+    use Test::WWW::Stub;
+
+# DESCRIPTION
+
+Test::WWW::Stub is ...
+
+# LICENSE
+
+Copyright (C) Asato Wakisaka.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Asato Wakisaka <asato.wakisaka@gmail.com>
---
+++ lib/Test/WWW/Stub.pm
@@ -0,0 +1,39 @@
+package Test::WWW::Stub;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::WWW::Stub - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::WWW::Stub;
+
+=head1 DESCRIPTION
+
+Test::WWW::Stub is ...
+
+=head1 LICENSE
+
+Copyright (C) Asato Wakisaka.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Asato Wakisaka E<lt>asato.wakisaka@gmail.comE<gt>
+
+=cut
+
